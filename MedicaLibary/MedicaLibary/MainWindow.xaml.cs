c8e2dbba4b8810c83273ed7c19dc9dca6645fe1c
Pindzia <rafpin@st.amu.edu.pl>
2016-05-31 02:32:47 +0200

    Prefinal problem
    Problem z decryptnięciem bóg gamedevu w nim nadzieja że zobaczy ten
    wspaniały błąd

---
+++ MedicaLibary/MedicaLibary/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace MedicaLibary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            XElementon.Instance.Initialize();
+        }
+
+        private void addPatient(object sender, RoutedEventArgs e)
+        {
+            RemoteFrame.Source = new Uri("AddPatientPage.xaml", UriKind.Relative);
+        }
+
+        private void getListPatient(object sender, RoutedEventArgs e)
+        {
+            RemoteFrame.Source = new Uri("GetListPage.xaml", UriKind.Relative);
+        }
+
+        private void searchEngine(object sender, RoutedEventArgs e)
+        {
+            RemoteFrame.Source = new Uri("SearchEngine.xaml", UriKind.Relative);
+        }
+
+        private void seekAndDelete(object sender, RoutedEventArgs e)
+        {
+            RemoteFrame.Source = new Uri("SeekAndDelete.xaml", UriKind.Relative);
+        }
+
+        private void killApp(object sender, RoutedEventArgs e)
+        {
+            XElementon.Instance.Disable();
+            Close();
+        }
+
+        
+    }
+}
