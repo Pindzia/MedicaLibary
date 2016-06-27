34e0ccb9a9afbc5a09149f142af34042ecb16cc8
Piolun <mojekontodospamowania@gmail.com>
2016-06-27 21:23:09 +0200

    Usunięcie Debugów v2

--- MedicaLibary/LastChanges.xaml.cs
+++ MedicaLibary/LastChanges.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Data;
 
 using System.Globalization; //CultureInfo
 using System.Windows.Markup; //MarkupExtensions
@@ -75,18 +78,46 @@ namespace MedicaLibary
     public partial class LastChanges : Page
     {
         //XElement database = XElement.Load(Environment.CurrentDirectory + "\\lib.xml");
-        //IEnumerable<XElement> result;
         XElement database = XElementon.Instance.getDatabase();
         
         public LastChanges()
         {
             InitializeComponent();
+
             DataGridChangelog.DataContext = database;
             DataGridChangelog.AutoGenerateColumns = false;
             //PatientsFrame.Source = new Uri("GetListPage.xaml", UriKind.Relative);
+            var resultPatients = from c in database.Descendants("patient")
+                                    select c;
+            var resultVisits = from d in database.Descendants("visit")
+                               select d;
+
+            string[] patients = new string [5];// {"1","2","3","4","5"};
+            string[] visits = new string[5];//{ "1", "2", "3" };
+
+            var result1 = from c in database.Descendants("patient_changes").Elements() select c;
+            var result2 = from c in database.Descendants("visit_changes").Elements() select c;
+
+            //foreach jeśli będziemy mieli zmienną listę zmian jednak.
+            for (int i = 0; i < 5; i++)
+            {
+                patients[i] = result1.ToArray()[i].Value;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                visits[i] = result2.ToArray()[i].Value;
+            }
+
+            resultPatients = resultPatients.Where(b => b.Elements("id").Any(f => patients.Contains((string)f)));
+            resultVisits = resultVisits.Where(b => b.Elements("idv").Any(f => visits.Contains((string)f)));
+            
+            DataGridPatients.DataContext = resultPatients;
+            DataGridPatients.AutoGenerateColumns = false;
+            DataGridVisits.DataContext = resultVisits;
+            DataGridVisits.AutoGenerateColumns = false;
         }
 
-        private void edit(object sender, RoutedEventArgs e)
+        private void editPatient(object sender, RoutedEventArgs e)
         { /*
             foreach (var item in Application.Current.Windows)
             {
@@ -95,7 +126,18 @@ namespace MedicaLibary
                     Uri newer = new Uri("EditSinglePatient.xaml", UriKind.Relative);
                     (item as MainWindow).RemoteFrame.Source = newer;
                 }
+            }
+            for (int i = DataGridPatients.SelectedItems.Count - 1; i >= 0; i--)
+            {
+                var a = (XElement)DataGridPatients.SelectedItems[0];
+                a.Remove();
+                MessageBox.Show("Usuwanie?");
             }*/
+        }
+
+        private void editVisit(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void Revert(object sender, RoutedEventArgs e)
