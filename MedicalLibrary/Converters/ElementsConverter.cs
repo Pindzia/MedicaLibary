daf047e40b4fba2fe8cb247c9f6f3595ad1b3abf
Pindzia <rafpin@st.amu.edu.pl>
2016-12-20 00:15:53 +0100

    Converters are unseen by VS

---
+++ MedicalLibrary/Converters/ElementsConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Xml.Linq;
+
+namespace MedicalLibrary.Converters
+{
+    public class ElementsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            XElement source = value as XElement;
+            if (source != null)
+            {
+                return source.Elements();
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
