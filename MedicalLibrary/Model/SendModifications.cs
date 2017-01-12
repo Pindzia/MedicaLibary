d1dcbcc73296ba1fad0609a5a7f9a171f2854674
Paweł <owocbananowca@gmail.com>
2017-01-12 20:40:21 +0100

    SendE
    Prawie działa?

--- MedicalLibrary/Model/SendModifications.cs
+++ MedicalLibrary/Model/SendModifications.cs
@@ -59,7 +59,7 @@ namespace MedicalLibrary.Model
                 }
 
                 var typdanych = (string)modyfikacja.Element("node_type");
-                var typoperacji = (string)modyfikacja.Element("node_type");
+                var typoperacji = (string)modyfikacja.Element("operation");
 
                 //Wszystko poza modyfiakcjami
                 if (typoperacji == "A")
@@ -252,9 +252,9 @@ namespace MedicalLibrary.Model
         {
             if (typdanych == "patient") //Pacjenci
             {
-                var pacjent = new PacjentToSendDTO();
-                var przypisanie = new Przypisanie_ParametruToSendDTO();
-                var listaprzypisan = new List<Przypisanie_ParametruToSendDTO>();
+                var pacjent = new PacjentNowyDTO();
+                var przypisanie = new Przypisanie_ParametruNowyDTO();
+                var listaprzypisan = new List<Przypisanie_ParametruNowyDTO>();
                 foreach (var zmiana in modyfikacja.Elements("newdata").Elements())
                 {
                     if (zmiana.Element("active") != null) //NotImplemented
@@ -288,29 +288,30 @@ namespace MedicalLibrary.Model
                     }
                     else if (zmiana.Name != "idp")
                     {
-                        przypisanie.id_pacjent = (int)modyfikacja.Element("newdata").Element("idp"); //Todo WutFace
-                        przypisanie.id_parametr = (int)XElementon.Instance.Field.Fields().Where(x => (string)x.Element("fieldname") == zmiana.Name).First().Element("idf"); //Todo WutFace
+                        przypisanie.id_pacjent = (int)modyfikacja.Element("newdata").Element("idp");  // nullException
+                        przypisanie.id_parametr = (int)XElementon.Instance.Field.Fields().Where(x => (string)x.Element("fieldname") == zmiana.Name).First().Element("idf");
                         przypisanie.wartosc = (string)zmiana;
-                        listaprzypisan.Add(przypisanie);
+                        //listaprzypisan.Add(przypisanie);
                     }
                 }
-
-                string uri = "/pacjent/" + idLekarz.ToString() + "/nowy";
-                await PushREST.UniversalPost(pacjent, uri);
-
+                pacjent.id = (int)modyfikacja.Element("id");
+                await PushREST.PacjentPut(pacjent, idLekarz);
+                /*
                 while (listaprzypisan.Any())
                 {
-                    uri = "/przypisanie/" + idLekarz.ToString() + "/nowy";
-                    await PushREST.UniversalPost(listaprzypisan[0], uri);
+                    //string uri = "/przypisanie/" + idLekarz.ToString() + "/nowy";
+                    //await PushREST.UniversalPost(listaprzypisan[0], uri);
+                    //listaprzypisan[0].id = ???;
+                    //await PushREST.PrzypisanieParametruPut(listaprzypisan[0],uri)
                     listaprzypisan.RemoveAt(0);
                 }
-
+                */
 
             }
 
             else if (typdanych == "visit") //Wizyty
             {
-                var wizyta = new WizytaToSendDTO();
+                var wizyta = new WizytaNowaDTO();
                 foreach (var zmiana in modyfikacja.Elements("newdata").Elements())
                 {
                     if (zmiana.Name.LocalName == "visit_addition_date")
@@ -326,13 +327,12 @@ namespace MedicalLibrary.Model
                         wizyta.komentarz = (string)zmiana;
                     }
                 }
-
-                string uri = "/wizyta/" + idLekarz.ToString() + "/nowa";
-                await PushREST.UniversalPost(wizyta, uri);
+                wizyta.id = (int) modyfikacja.Element("id");
+                await PushREST.WizytaPut(wizyta, idLekarz);
             }
             else if (typdanych == "storehouse") //Magazyny
             {
-                var magazyn = new MagazynToSendDTO();
+                var magazyn = new MagazynNowyDTO();
                 foreach (var zmiana in modyfikacja.Elements("newdata").Elements())
                 {
                     if (zmiana.Name == "size")
@@ -348,12 +348,12 @@ namespace MedicalLibrary.Model
                         magazyn.priorytet = Convert.ToInt32((string)zmiana);
                     }
                 }
-                string uri = "/magazyn/" + idLekarz.ToString() + "/nowy";
-                await PushREST.UniversalPost(magazyn, uri);
+                magazyn.id = (int)modyfikacja.Element("id");
+                await PushREST.MagazynPut(magazyn, idLekarz);
             }
             else if (typdanych == "rule") //Zasady
             {
-                var zasada = new ZasadaToSendDTO();
+                var zasada = new ZasadaNowaDTO();
                 zasada.id_magazynu = Convert.ToInt32((string)modyfikacja.Element("ids")); //Wololocode
                 foreach (var zmiana in modyfikacja.Elements("newdata").Elements())
                 {
@@ -374,12 +374,12 @@ namespace MedicalLibrary.Model
                         zasada.wartosc_porownania = (string)zmiana;
                     }
                 }
-                string uri = "/zasada/" + idLekarz.ToString() + "/nowy";
-                await PushREST.UniversalPost(zasada, uri);
+                zasada.id = (int) modyfikacja.Element("id");
+                await PushREST.ZasadaPut(zasada, idLekarz);
             }
             else if (typdanych == "field") //Pola
             {
-                var parametr = new ParametrToSendDTO();
+                var parametr = new ParametrNowyDTO();
                 foreach (var zmiana in modyfikacja.Elements("newdata").Elements())
                 {
                     if (zmiana.Name == "fieldname")
@@ -395,8 +395,8 @@ namespace MedicalLibrary.Model
                         parametr.wartosc_domyslna = (string)zmiana;
                     }
                 }
-                string uri = "/parametr/" + idLekarz.ToString() + "/nowy";
-                await PushREST.UniversalPost(parametr, uri);
+                parametr.id = (int) modyfikacja.Element("id");
+                await PushREST.ParametrPut(parametr, idLekarz);
             }
         }
 
