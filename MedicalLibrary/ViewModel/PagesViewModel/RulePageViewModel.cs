989fbf1e2516d4d771c29c12e96c3a3ac2225e96
Pindzia <rafpin@st.amu.edu.pl>
2017-01-21 18:46:19 +0100

    Work with RulePage

---
+++ MedicalLibrary/ViewModel/PagesViewModel/RulePageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml.Linq;
+
+namespace MedicalLibrary.ViewModel.PagesViewModel
+{
+    public class RulePageViewModel : BaseViewModel
+    {
+        public RulePageViewModel()
+        {
+
+        }
+
+       private ObservableCollection<XElement> _Propname = new ObservableCollection<XElement>();
+       public ObservableCollection<XElement> Propname
+       {
+            get
+            {
+                 return _Propname;
+            }
+            set
+            {
+                 _Propname=value;
+                 OnPropertyChanged(nameof(Propname));
+            }
+       }
+       
+
+
+
+        public ICommand AddRuleToMagazine { get; set; }
+        public ICommand DeleteRuleFromMagazine { get; set; }
+        public ICommand AddRule { get; set; }
+        public ICommand EditRule { get; set; }
+        public ICommand DeleteRule { get; set; }
+    }
+}
