fbe3829c8ba8e3e3117b12efe633868c9f0af3e1
Pindzia <rafpin@st.amu.edu.pl>
2017-01-26 00:00:51 +0100

    Fix to do

---
+++ MedicalLibrary/ViewModel/PagesViewModel/ExpiredPatientsPageViewModel.cs
@@ -0,0 +1,93 @@
+﻿using MedicalLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MedicalLibrary.ViewModel.PagesViewModel
+{
+    public class ExpiredPatientsPageViewModel :BaseViewModel
+    {
+        public ExpiredPatientsPageViewModel()
+        {
+            UpdateData();
+            DestroyPatient = new RelayCommand(pars => What());
+            FixStorehouse = new RelayCommand(pars => Fix());
+            LoadedCommand = new RelayCommand(pars => Loaded());
+        }
+
+        private void UpdateData()
+        {
+            ExpiredPatients = ObserverCollectionConverter.Instance.Observe(XElementon.Instance.Patient.InWrongStorehouse());
+        }
+
+        private ObservableCollection<XElement> _ExpiredPatients = new ObservableCollection<XElement>();
+        public ObservableCollection<XElement> ExpiredPatients
+        {
+            get
+            {
+                return _ExpiredPatients;
+            }
+
+            set
+            {
+                _ExpiredPatients = value;
+                OnPropertyChanged("ExpiredPatients");
+            }
+        }
+
+        private XElement _SelectedItem = null;
+        public XElement SelectedItem
+        {
+            get
+            {
+                return _SelectedItem;
+            }
+
+            set
+            {
+                _SelectedItem = value;
+                OnPropertyChanged("SelectedItem");
+            }
+        }
+
+
+        public ICommand DestroyPatient { get; set; }
+        public ICommand FixStorehouse { get; set; }
+        public ICommand LoadedCommand { get; set; }
+
+        private void What()
+        {
+            if (SelectedItem != null)
+            {
+                Tuple<string, string> Answer = XElementon.Instance.Patient.WhatStorehouseEnvelope((int)SelectedItem.Element("idp"));
+                MessageBox.Show("Powinno sie przenieś wybranego pacjenta do magazynu o nazwie: \n" + Answer.Item1 + "\nw kopercie o numerze: " + Answer.Item2);
+            }
+            else
+            {
+                MessageBox.Show("Wybierz Pacjenta by dowiedzieć się do jakiego magazynu należy");
+            }
+        }
+
+        private void Fix()
+        {
+            if (SelectedItem != null)
+            {
+                XElementon.Instance.Patient.FixStorehouseEnvelope((int)SelectedItem.Element("idp"));
+                UpdateData();
+            }
+            else
+            {
+                MessageBox.Show("Wybierz Pacjenta by naprawić jego Magazyn i Kopertę");
+            }
+        }
+
+        private void Loaded()
+        {
+            UpdateData();
+        }
+    }
+}
